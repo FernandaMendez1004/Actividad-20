8ab5b037e1ccfb4e5d52026e48d602b65c6914d5
verno <verno@ASUS>
2026-04-09 21:23:05 -0600

    ej 03

--- Ejercicio 03/Program.cs
+++ Ejercicio 03/Program.cs
@@ -1,1 +1,24 @@
-﻿Console.WriteLine("Hello, World!");
+﻿int cantedad = 0, suma = 0;
+bool correcto;
+do
+{
+    Console.WriteLine("Ingrese cantidad de numeros desea ingresar");
+    correcto = int.TryParse(Console.ReadLine(), out cantedad);
+    if (!correcto)
+    {
+        Console.WriteLine("Ingrese un numero valido");
+    }
+    else if (cantedad <= 0)
+    {
+        Console.WriteLine("Ingrese un numero mayor a cero");
+        correcto = false;
+    }
+} while (!correcto || cantedad <= 0);
+int[] numeros = new int[cantedad];
+for (int i = 0; i < numeros.Length; i++)
+{
+    Console.WriteLine("Ingrese el numero");
+    numeros[i] = int.Parse(Console.ReadLine());
+    suma += numeros[i];
+}
+Console.WriteLine("La suma de los numeros es: " + suma);
