bcc8b328e85a8341aa53e59eca3c4e158fa14f85
verno <verno@ASUS>
2026-04-09 21:10:49 -0600

    ejercicio 01

---
+++ Ejercicio 01/Program.cs
@@ -0,0 +1,27 @@
+﻿int cantedad = 0;
+bool correcto;
+do
+{
+    Console.WriteLine("Ingrese cantidad de numeros desea ingresar");
+    correcto = int.TryParse(Console.ReadLine(), out cantedad);
+    if (!correcto)
+    {
+        Console.WriteLine("Ingrese un numero valido");
+    }
+    else if (cantedad <= 0)
+    {
+        Console.WriteLine("Ingrese un numero mayor a cero");
+        correcto = false;
+    }
+} while (!correcto || cantedad <= 0);
+int[] numeros = new int[cantedad];
+for (int i = 0; i < numeros.Length; i++)
+{
+    Console.WriteLine($"Ingrese el numero");
+    numeros[i] = int.Parse(Console.ReadLine());
+}
+Console.WriteLine("Los numeros son");
+for (int i = 0; i < numeros.Length; i++)
+{
+    Console.WriteLine(numeros[i]);
+}
