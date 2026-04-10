807ac4d0f28c428bea0caafb87feec6b155ee007
verno <verno@ASUS>
2026-04-09 21:25:51 -0600

    ej 04

---
+++ Ejercicio 04/Program.cs
@@ -0,0 +1,25 @@
+﻿int cantedad = 0, suma = 0, prom = 0;
+bool correcto;
+do
+{
+    Console.WriteLine("Ingrese cantidad de numeros desea ingresar");
+    correcto = int.TryParse(Console.ReadLine(), out cantedad);
+    if (!correcto)
+    {
+        Console.WriteLine("Ingrese un numero valido");
+    }
+    else if (cantedad <= 0)
+    {
+        Console.WriteLine("Ingrese un numero mayor a cero");
+        correcto = false;
+    }
+} while (!correcto || cantedad <= 0);
+int[] numeros = new int[cantedad];
+for (int i = 0; i < numeros.Length; i++)
+{
+    Console.WriteLine("Ingrese el numero");
+    numeros[i] = int.Parse(Console.ReadLine());
+    suma += numeros[i];
+}
+prom = suma / cantedad;
+Console.WriteLine("el promedio de los numeros es: " + prom);
